bdff251bcbdfae73ff99323a9a4e9351784a16c7
MintyWorks <mintymintworks@gmail.com>
2026-07-10 16:29:46 +0700

    Update DisablePluginsCommand.java
    Cleaner ig

--- src/main/java/com/nikolaipatrick/disablepluginscommand/DisablePluginsCommand.java
+++ src/main/java/com/nikolaipatrick/disablepluginscommand/DisablePluginsCommand.java
@@ -1,14 +1,13 @@
 package com.nikolaipatrick.disablepluginscommand;
 
-import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DisablePluginsCommand extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Loading disablePluginsCommand V1.1.2");
-        getLogger().info("DisablePluginsCommand by Nikolai Patrick 2022-2023");
+        getLogger().info("Loading DisablePluginsCommand v" + getDescription().getVersion());
+        getLogger().info("DisablePluginsCommand by Nikolai Patrick 2022-2026");
         getServer().getPluginManager().registerEvents(new commandListener(), this);
         getCommand("disablepluginscmd").setExecutor(new commandDisablepluginscmd());
         getCommand("disablepluginscmd").setTabCompleter(new cmdTabCompleter());
@@ -18,6 +17,5 @@ public final class DisablePluginsCommand extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Goodbye!");
-
     }
 }
